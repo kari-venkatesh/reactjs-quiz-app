7cbde5ac637606395b1b9ff0dea9abb3bea3241f
ManassehMwangi <manassehmwangi9@gmail.com>
2024-05-30 14:55:17 +0300

    domain acquired

--- quiz-app/src/Components/Quiz/Quiz.jsx
+++ quiz-app/src/Components/Quiz/Quiz.jsx
@@ -24,7 +24,8 @@ const Quiz = () => {
         try {
           // const apiUrl = import.meta.env.VITE_REACT_APP_API_URL || 'http://localhost:3000/api/questions';
           //const apiUrl = "http://quizapp.cloudcorehub.com/api/questions"
-          const apiUrl = "http://localhost:3000/api/questions"
+          //http://www.devatlas.org/
+          const apiUrl = "http://quizapp.devatlas.org/api/questions"
           const response = await fetch(apiUrl);
           if (!response.ok) throw new Error("Failed to fetch");
           let questions = await response.json();
